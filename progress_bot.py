3057ee2c9d1cf744194562218cabdcf4eb0fb69f
danman55575 <100946635+danman55575@users.noreply.github.com>
2022-04-02 20:47:05 +0300

    Update progress_bot.py

--- progress_bot.py
+++ progress_bot.py
@@ -1,7 +1,7 @@
 import logging
 from aiogram import executor
 from bot_manager import dp
-from handlers import maincourse, menu, admin, comment
+from handlers import maincourse, menu, admin, review
 
 
 logging.basicConfig(level=logging.INFO)
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 menu.register_handlers_menu(dp)
 maincourse.register_handlers_maincourse(dp)
-comment.register_handlers_comment(dp)
+comment.register_handlers_review(dp)
 admin.register_handlers_admin(dp)
 
 
